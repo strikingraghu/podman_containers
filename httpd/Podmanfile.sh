79c6afb108b041595bcb6ffd382034442509356b
strikingraghu <strikingraghu@gmail.com>
2022-01-31 19:38:26 +0530

    Buildah build files are added into repo

---
+++ httpd/Podmanfile.sh
@@ -0,0 +1,11 @@
+container=$(buildah from fedora:latest)
+buildah config --label maintainer="buildah@podman.rulez" $container
+
+buildah run $container dnf install -y httpd
+buildah run $container dnf clean all
+
+buildah run $container bash -c "echo \"Welcome to Podman container\" > /var/www/html/welcome
+
+buildah config --port 80 $container
+buildah config --cmd "/usr/sbin/httpd -DFOREGROUND" $container
+buildah commit --format docker $container buildah-fedora-httpd:latest
