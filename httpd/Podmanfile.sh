f08ad45a95c6f450cc9082f090ceae74c9638799
strikingraghu <strikingraghu@gmail.com>
2022-01-31 22:06:23 +0530

    Changed settings in Podmanfile.sh build script

--- httpd/Podmanfile.sh
+++ httpd/Podmanfile.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
+
 container=$(buildah from fedora:latest)
 buildah config --label maintainer="buildah@podman.rulez" $container
 
 buildah run $container dnf install -y httpd
 buildah run $container dnf clean all
 
-buildah run $container bash -c "echo \"Welcome to Podman container\" > /var/www/html/welcome
+buildah run $container bash -c "echo \"Welcome to Podman container\" > /var/www/html/welcome"
 
 buildah config --port 80 $container
 buildah config --cmd "/usr/sbin/httpd -DFOREGROUND" $container
-buildah commit --format docker $container buildah-fedora-httpd:latest
+buildah commit --format docker $container buildah-httpd:latest
